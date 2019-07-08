923e18afcedd01035315d0e0050c2e16ab4693d1
Anne Marijke Dusseljee <51442838+AnneMarijke@users.noreply.github.com>
2019-07-08 02:18:11 +0200

    Create saltstack-master.sh

---
+++ SaltStack-Installation-Scripts/saltstack-master.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+"deb http://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest bionic main" >> /etc/apt/sources.list.d/saltstack.list
+sudo apt-get update --allow-insecure-repositories
+sudo apt-get install salt-master salt-minion
+sudo systemctl start salt-master
+sudo systemctl enable salt-master
+sudo systemctl start salt-minion
+sudo systemctl enable salt-minion
+sudo firewall-cmd --permanent --zone=public --add-port=4505-4506/tcp
+sudo firewall-cmd –reload
+sudo salt-key –A
+mkdir /srv/salt
